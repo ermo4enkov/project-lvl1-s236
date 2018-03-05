7886f9849c9b4f7c49a1f6c192077b6b29d44611
ermojob <ermojob@gmail.com>
2018-03-05 18:19:43 +0200

    Исправил решение

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
 import Index from '..';
+
+Index();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readlineSync from "readline-sync";
 import colors from "colors";
 
-console.log("Welcome to the " + "Brain ".bold + "Games!\n\n");
-const userName  = readlineSync.question('May I have your name? ');
-console.log("Hello, " + userName.bold);
+export default () => {
+	console.log("Welcome to the " + "Brain ".bold + "Games!\n\n");
+	const userName  = readlineSync.question('May I have your name? ');
+	console.log("Hello, " + userName.bold);
+}
