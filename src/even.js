770aabe828016a935a061bb88c014b05e2afcd20
ermojob <ermojob@gmail.com>
2018-03-06 18:17:19 +0200

    Finished with programm

--- src/even.js
+++ src/even.js
@@ -6,30 +6,34 @@ const isNumberEven = number => number % 2 === 0;
 
 const checkAnswer = (answer, number) => (isNumberEven(number) && answer === 'yes') || (!isNumberEven(number) && answer === 'no');
 
-const findOutNumber = () => {
-  const number = getRandomNumber();
-  console.log(`Question: ${number}`);
-  const answer = readlineSync.question('Your answer: ');
-  console.log(checkAnswer(answer, number));
+const switchAnswer = (number) => {
+  return isNumberEven(number) ? 'yes' : 'no';
+};
+
+const findOutNumber = (userName) => {
+  let count = 0;
+  while (count < 3) {
+    const number = getRandomNumber();
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (checkAnswer(answer, number)) {
+      count += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${switchAnswer(number)}'`);
+      console.log(`Let's try again, ${userName}!`);
+      return false;
+    }
+  }
+  console.log('Correct!');
+  console.log(`Congratulations ${userName}`);
 };
 
 const evenOrNoGame = () => {
-  console.log('Welcome to the Brain Games!\n\n');
-  console.log('Answer "yes" if number even otherwise answer "no"');
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}`);
-  findOutNumber();
+  console.log(`Hello, ${userName}\n`);
+  findOutNumber(userName);
 };
 
 export default evenOrNoGame;
-
-// Question: 15
-// Your answer: no
-// Correct!
-// Question: 6
-// Your answer: yes
-// Correct!
-// Question: 7
-// Your answer: no
-// Correct!
-// Congratulations, Sam!
