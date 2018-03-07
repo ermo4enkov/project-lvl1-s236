c327344b364d684e93b4068e297aa8d163bb700b
ermojob <ermojob@gmail.com>
2018-03-07 13:53:44 +0200

    Finished

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,49 +1,36 @@
 import readlineSync from 'readline-sync';
-import { cons, car, cdr } from 'hexlet-pairs';
 import chalk from 'chalk';
-import { getRandomNumber, sayHello } from '../utils';
+import { getRandomNumber, sayHello, checkNumberAnswer } from '../utils';
 
 const randomNumb = () => getRandomNumber();
 const greetings = subtitle => sayHello(subtitle);
 
-const checkAnswer = (goal, answer) => Number(goal) === Number(answer);
+const expectedAnswer = (num1, num2) => {
+  if (!num2) {
+    return num1;
+  }
+  return expectedAnswer(num2, num1 % num2);
+};
 
-// const expectedAnswer = (num1, num2, sign) => {
-//   switch (String(sign)) {
-//     case '+':
-//       return Number(num1) + Number(num2);
-//     case '-':
-//       return Number(num1) - Number(num2);
-//     case '*':
-//       return Number(num1) * Number(num2);
-//     default:
-//       return false;
-//   }
-// };
+const checkAnswer = (goal, answer) => checkNumberAnswer(goal, answer);
 
 const findOutGCD = (userName) => {
-  const pairOfNumbers = cons(randomNumb(), randomNumb());
-  const num1 = car(pairOfNumbers);
-  const num2 = cdr(pairOfNumbers);
-  console.log(`Question: ${num1}${num2}`);
-
-//   let count = 0;
-//   while (count < 3) {
-//     const pairOfNumbers = cons(randomNumb(), randomNumb());
-//     const num1 = car(pairOfNumbers);
-//     const num2 = cdr(pairOfNumbers);
-//     const goal = expectedAnswer(num1, num2);
-//     console.log(`Question: ${num1}${num2}`);
-//     const answer = readlineSync.question(`${chalk.hex('#d14')('Your')} answer: `).toLowerCase();
-//     if (checkAnswer(goal, answer)) {
-//       count += 1;
-//       console.log(`${chalk.hex('#d14')('Correct!')}`);
-//     } else {
-//       console.log(`'${chalk.hex('#990073')(answer)}' is wrong answer ;(. Correct answer was '${chalk.hex('#990073')(goal)}'`);
-//       console.log(`Let's try again, ${userName}!`);
-//       return false;
-//     }
-//   }
+  let count = 0;
+  while (count < 3) {
+    const num1 = randomNumb();
+    const num2 = randomNumb();
+    const goal = expectedAnswer(num1, num2);
+    console.log(`Question: ${num1} ${num2}`);
+    const answer = readlineSync.question(`${chalk.hex('#d14')('Your')} answer: `).toLowerCase();
+    if (checkAnswer(goal, answer)) {
+      count += 1;
+      console.log(`${chalk.hex('#d14')('Correct!')}`);
+    } else {
+      console.log(`'${chalk.hex('#990073')(answer)}' is wrong answer ;(. Correct answer was '${chalk.hex('#990073')(goal)}'`);
+      console.log(`Let's try again, ${userName}!`);
+      return false;
+    }
+  }
 
   console.log(`${chalk.hex('#d14')('Congratulations,', userName, '!')}`);
   return true;
