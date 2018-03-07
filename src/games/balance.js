8bcc9926d3375b8a35424748d4f8a0b30adf1ea9
ermojob <ermojob@gmail.com>
2018-03-07 17:43:08 +0200

    #module7 doing a lot of work to separate instruments

--- src/games/balance.js
+++ src/games/balance.js
@@ -4,39 +4,26 @@ import { getRandomNumber, sayHello, checkNumberAnswer } from '../utils';
 
 const randomNumb = () => getRandomNumber();
 const greetings = subtitle => sayHello(subtitle);
-const getSumOfNumbers = arr => arr.reduce(function(sum, current) {
+const addNumbers = function (sum, current) {
   return Number(sum) + Number(current);
-}, 0);
+};
+const totalSumOfNumbers = arr => arr.reduce(addNumbers, 0);
 
+const getSummOfNumbers = (number) => {
+  const num = String(number);
+  const numbersArray = num.split('');
+  return totalSumOfNumbers(numbersArray);
+};
 
-
-
-const getBalancedNumber = number => console.log(String(number).length);
+const getBalancedNumber = (number) => {
+  const summ = getSummOfNumbers(number);
+  console.log(summ);
+};
 
 const findOutBalance = (userName) => {
   console.log(userName);
   getBalancedNumber(215);
 };
-
-const starterNum = (length) => {
-  const startedNum = [];
-  for (let i = 0; i < length; i += 1) {
-    startedNum.push(0);
-  }
-  startedNum[startedNum.length - 1] = 1;
-  return startedNum.join;
-};
-
-
-
-
-
-
-
-
-
-
-
 
 const balanceGame = () => {
   greetings('Find the greatest common divisor of given numbers.');
