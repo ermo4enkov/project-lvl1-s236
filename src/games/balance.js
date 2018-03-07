15feb68c86728e3500f3b9a87c1a95478e7f3ebd
ermojob <ermojob@gmail.com>
2018-03-07 16:40:48 +0200

    #module7 added started function for getting number

--- src/games/balance.js
+++ src/games/balance.js
@@ -4,10 +4,32 @@ import { getRandomNumber, sayHello, checkNumberAnswer } from '../utils';
 
 const randomNumb = () => getRandomNumber();
 const greetings = subtitle => sayHello(subtitle);
+const getBalancedNumber = number => console.log(String(number).length);
 
 const findOutBalance = (userName) => {
   console.log(userName);
+  getBalancedNumber(215);
 };
+
+const getbalance = (length) => {
+  const startedNum = [];
+  for (let i = 0; i < length; i += 1) {
+    startedNum.push(0);
+  }
+  startedNum[startedNum.length - 1] = 1;
+  return startedNum.join;
+};
+
+
+
+
+
+
+
+
+
+
+
 
 const balanceGame = () => {
   greetings('Find the greatest common divisor of given numbers.');
