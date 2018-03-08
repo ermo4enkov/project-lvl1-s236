ef252da9ccf26d372b8c81a73c82b8a5df85ea89
ermojob <ermojob@gmail.com>
2018-03-08 17:04:21 +0200

    #master finished balance

--- src/games/balance.js
+++ src/games/balance.js
@@ -5,19 +5,18 @@ import { getRandomNumber } from '../utils';
 const gameTitle = 'Balance the given number.';
 
 const getBalanceNumber = (number) => {
-  const integers = String(number).split('').sort((a, b) => a - b).join('');
-  const min = Number(integers.charAt(0));
-  const max = Number(integers.charAt(integers.length - 1));
-  const remainder = max - min;
-  if (remainder < 2) {
-    return integers;
+  const integers = String(number).split('').sort((a, b) => a - b);
+  const length = integers.length - 1;
+  const first = Number(integers[0]);
+  const last = Number(integers[length]);
+  if (last - first <= 1) {
+    return integers.join('');
   }
-  const balance = (remainder - (remainder % 2)) / 2;
-  let restStr = '';
-  if (integers.length > 2) {
-    restStr = integers.substring(1, integers.length - 2);
-  }
-  return getBalanceNumber(`${min + balance}${restStr}${max - balance}`);
+  integers[0] = first + 1;
+  integers[length] = last - 1;
+  const result = Number(integers.join(''));
+
+  return getBalanceNumber(result);
 };
 
 
