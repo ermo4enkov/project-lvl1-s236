426ed855695ee8553ad4d0a04caeebe27ef2c8bb
ermojob <ermojob@gmail.com>
2018-03-08 14:28:02 +0200

    #master improved even, calc, gcd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import readlineSync from 'readline-sync';
 import chalk from 'chalk';
-import { car, cdr } from 'hexlet-pairs';
+import readlineSync from 'readline-sync';
 
-import { sayHello, checkAnswer } from './utils';
+import { sayHello, startGame } from './utils';
 
 export const getUserName = () => {
   const name = readlineSync.question(`May I have your ${chalk.hex('#0086b3')('name')}? `);
@@ -13,25 +12,7 @@ export const getUserName = () => {
 const gameInterface = (gameTitle, gamePlay) => {
   sayHello(`${gameTitle}`);
   const userName = getUserName();
-  let count = 0;
-  while (count < 3) {
-    const game = gamePlay(car, cdr);
-    const gameGoal = String(cdr(game));
-    const gameQuestion = car(game);
-    console.log(`Question: ${gameQuestion}`);
-    const answer = readlineSync.question(`${chalk.hex('#d14')('Your')} answer: `).toLowerCase();
-    if (checkAnswer(gameGoal, answer)) {
-      count += 1;
-      console.log(`${chalk.hex('#d14')('Correct!')}`);
-    } else {
-      console.log(`'${chalk.hex('#990073')(answer)}' is wrong answer ;(. Correct answer was '${chalk.hex('#990073')(gameGoal)}'`);
-      console.log(`Let's try again, ${userName}!`);
-      return false;
-    }
-  }
-  console.log(`${chalk.hex('#d14')('Congratulations,', userName, '!')}`);
-  return true;
+  startGame(gameTitle, gamePlay, userName);
 };
 
 export default gameInterface;
-
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
+import chalk from 'chalk';
+import readlineSync from 'readline-sync';
+import { car, cdr } from 'hexlet-pairs';
+
 export const getRandomNumber = (min = 1, max = 100) =>
   Math.floor(Math.random() * ((max - min) + 1)) + min;
 
@@ -7,3 +11,24 @@ export const getRandomItem = arr => arr[Math.floor(Math.random() * arr.length)];
 
 export const checkAnswer = (goal, answer) => goal === answer;
 
+
+export const startGame = (gameTitle, gamePlay, userName) => {
+  let count = 0;
+  while (count < 3) {
+    const game = gamePlay(car, cdr);
+    const gameGoal = String(cdr(game));
+    const gameQuestion = car(game);
+    console.log(`Question: ${gameQuestion}`);
+    const answer = readlineSync.question(`${chalk.hex('#d14')('Your')} answer: `).toLowerCase();
+    if (checkAnswer(gameGoal, answer)) {
+      count += 1;
+      console.log(`${chalk.hex('#d14')('Correct!')}`);
+    } else {
+      console.log(`'${chalk.hex('#990073')(answer)}' is wrong answer ;(. Correct answer was '${chalk.hex('#990073')(gameGoal)}'`);
+      console.log(`Let's try again, ${userName}!`);
+      return false;
+    }
+  }
+  console.log(`${chalk.hex('#d14')('Congratulations,', userName, '!')}`);
+  return true;
+};
