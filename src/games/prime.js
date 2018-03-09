408e709f723d205a97097b23dc97512ae25dc23e
ermojob <ermojob@gmail.com>
2018-03-09 23:13:19 +0200

    #master improve prime, again

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,20 +6,20 @@ const gameTitle = 'Is this number prime?';
 
 const checkNumberPrime = (number, step = 2) => {
   if (number < 2) {
-    return 'no';
+    return false;
   }
   if (number % step === 0) {
-    return 'no';
+    return false;
   }
   if (step >= number / 2) {
-    return 'yes';
+    return true;
   }
   return checkNumberPrime(number, step + 1);
 };
 
 const gamePlayPrime = () => {
   const number = getRandomNumber();
-  const gameGoal = checkNumberPrime(number);
+  const gameGoal = checkNumberPrime(number) ? 'yes' : 'no';
   const gameQuestion = `${number}`;
   return cons(gameQuestion, gameGoal);
 };
